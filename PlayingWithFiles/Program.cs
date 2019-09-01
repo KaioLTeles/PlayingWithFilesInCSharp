1ec57e131312dc4e1bc4fa8886b692ef5ca32d14
Kaio Teles <kaiolunateles@gmail.com>
2019-09-01 08:39:19 -0300

    Simple exercice that coppy and read a file.

---
+++ PlayingWithFiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace PlayingWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+                //Creating object fileInfor
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                //Coppy a file in temp directory
+                fileInfo.CopyTo(targetPath);
+                //Creating a array that recive the content of the file1 arquive.
+                string[] lines = File.ReadAllLines(sourcePath);
+                //Printing in console the file content
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
