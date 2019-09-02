e8a1100e774875b468473760be2b5d3210894d1d
Kaio Teles <kaiolunateles@gmail.com>
2019-09-02 19:35:12 -0300

    Creating the StreamReader and FileStream object to read a file.

--- PlayingWithFiles/Program.cs
+++ PlayingWithFiles/Program.cs
@@ -7,27 +7,24 @@ namespace PlayingWithFiles
     {
         static void Main(string[] args)
         {
-            string sourcePath = @"C:\temp\file1.txt";
-            string targetPath = @"C:\temp\file2.txt";
+            string path = @"C:\temp\file1.txt";
+            StreamReader sr = null;
 
             try
-            {
-                //Creating object fileInfor
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                //Coppy a file in temp directory
-                fileInfo.CopyTo(targetPath);
-                //Creating a array that recive the content of the file1 arquive.
-                string[] lines = File.ReadAllLines(sourcePath);
-                //Printing in console the file content
-                foreach (string line in lines)
-                {
-                    Console.WriteLine(line);
-                }
+            {                
+                sr = new StreamReader( new FileStream(path, FileMode.Open)); // Instaciando dentro do StreamReader o FileStream
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
             }
             catch(IOException e)
             {
                 Console.WriteLine("An error occurred!");
                 Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                
             }
         }
     }
