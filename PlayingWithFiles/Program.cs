532fc400704ef0af9ac99ac381d7be9a8692307d
Kaio Teles <kaiolunateles@gmail.com>
2019-09-02 20:26:20 -0300

    Including the using element for close my disposable objects.

--- PlayingWithFiles/Program.cs
+++ PlayingWithFiles/Program.cs
@@ -8,28 +8,29 @@ namespace PlayingWithFiles
         static void Main(string[] args)
         {
             string path = @"C:\temp\file1.txt";
-            StreamReader sr = null;
 
             try
             {
-                sr = File.OpenText(path);
-                //Percorre todo o arquivo até o final dele
-                while (!sr.EndOfStream)
+                using (FileStream fs = new FileStream(path, FileMode.Open))
                 {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
                 }
             }
-            catch (IOException e)
+            catch(IOException e)
             {
                 Console.WriteLine("An error occurred!");
                 Console.WriteLine(e.Message);
             }
-            finally
-            {
-                if (sr != null) sr.Close();
 
-            }
+
+
         }
     }
 }
